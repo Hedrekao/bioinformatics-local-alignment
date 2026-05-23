0c5bc9e032321cbe1a95f6d9e836bf2ccefb2e1d
hedrekao <82450784+Hedrekao@users.noreply.github.com>
2026-05-23 16:31:37 +0900

    implement affine gap penalty

--- main.zig
+++ main.zig
@@ -23,6 +23,7 @@ const AminoAcid = enum(u5) {
     V = 19,
 };
 
+// BLOSUM62 scoring matrix
 const Blosum62 = struct {
     matrix: [20][20]i8,
 
@@ -80,6 +81,10 @@ const Blosum62 = struct {
 
 const BLOSUM62: Blosum62 = .init();
 
+// we use affine gap penalty where opening the gap has a high penalty and extending the gap a lower one
+var GAP_OPEN_PENALTY: i32 = -11;
+var GAP_EXTEND_PENALTY: i32 = -1;
+
 // at each position of alignment we either have a match/mismatch, an insertion or a deletion. We can represent these with an enum and store the type of alignment along with the score and the index of the previous cell in the matrix to allow traceback for building the final alignment.
 const AlignmentType = union(enum) {
     MatchMismatch: usize,
@@ -87,95 +92,196 @@ const AlignmentType = union(enum) {
     Deletion,
 };
 
-// entry in the dynamic programming matrix during global alignment process
-const Entry = struct { score: i32, prev_idx: usize, alignment_type: AlignmentType };
+// entry in the dynamic programming matrix during global alignment process with affine gap penalty
+const MatrixState = enum { M, I, D };
+const Entry = struct { score: i32, prev_idx: usize, prev_matrix: MatrixState, alignment_type: AlignmentType };
 
 const Alignment = []AlignmentType;
 
+// result of global alignment containing the final score and the alignment itself
 const AlignmentResult = struct {
     score: i32,
     alignment: Alignment,
 };
 
+// the star sequence is the sequence that has the highest total score when aligned to all other sequences. We store the index of the star sequence, its alignments to all other sequences and the maximum score for later use in building the multi-alignment.
 const StarSequence = struct {
     idx: usize,
     alignments: []Alignment,
     max_score: i32,
 };
 
+// global aligment with affine gap penalty following Gotoh's algorithm' (more optimized version of Smith-Waterman that works better with affine gap penalty)
 fn globalAlignment(allocator: std.mem.Allocator, query: []const u8, subj: []const u8) !AlignmentResult {
     const query_len = query.len + 1;
     const subj_len = subj.len + 1;
 
-    var matrix = try allocator.alloc(Entry, query_len * subj_len);
-    defer allocator.free(matrix);
+    const neg_inf: i32 = -999999;
 
-    // Initialize boundary conditions for global alignment (gap penalties)
-    for (0..query_len) |j| {
-        matrix[0 * query_len + j] = .{
-            .score = -@as(i32, @intCast(j)),
-            .prev_idx = if (j > 0) j - 1 else 0,
-            .alignment_type = .Deletion,
-        };
-    }
-    for (0..subj_len) |i| {
-        const prev_idx = if (i > 0) (i - 1) * query_len else 0;
-        matrix[i * query_len + 0] = .{
-            .score = -@as(i32, @intCast(i)),
-            .prev_idx = prev_idx,
-            .alignment_type = .{ .Insertion = if (i > 0) i - 1 else 0 },
-        };
+    // we maintain three matrices M, I, D
+    // M[i][j] = best score for aligning query[0..i] and subj[0..j] ending in a match/mismatch
+    // I[i][j] = best score for aligning query[0..i] and subj[0..j] ending in an insertion (gap in query)
+    // D[i][j] = best score for aligning query[0..i] and subj[0..j] ending in a deletion (gap in subj)
+    var M = try allocator.alloc(Entry, query_len * subj_len);
+    defer allocator.free(M);
+
+    var I = try allocator.alloc(Entry, query_len * subj_len);
+    defer allocator.free(I);
+
+    var D = try allocator.alloc(Entry, query_len * subj_len);
+    defer allocator.free(D);
+
+    // 1. Initialize Top-Left Cell (i=0, j=0)
+    M[0] = .{ .score = 0, .prev_idx = 0, .prev_matrix = .M, .alignment_type = .{ .MatchMismatch = 0 } };
+    I[0] = .{ .score = neg_inf, .prev_idx = 0, .prev_matrix = .I, .alignment_type = .{ .Insertion = 0 } };
+    D[0] = .{ .score = neg_inf, .prev_idx = 0, .prev_matrix = .D, .alignment_type = .Deletion };
+
+    // 2. Initialize Top Boundary (j > 0, i=0)
+    for (1..query_len) |j| {
+        const idx = 0 * query_len + j;
+        const gap_penalty = GAP_OPEN_PENALTY + @as(i32, @intCast(j - 1)) * GAP_EXTEND_PENALTY;
+
+        // just a placeholder as deletions only matter in actual deletion matrix
+        M[idx] = .{ .score = neg_inf, .prev_idx = 0, .prev_matrix = .M, .alignment_type = .Deletion };
+        I[idx] = .{ .score = neg_inf, .prev_idx = 0, .prev_matrix = .I, .alignment_type = .{ .Insertion = 0 } };
+
+        D[idx] = .{ .score = gap_penalty, .prev_idx = j - 1, .prev_matrix = if (j == 1) .M else .D, .alignment_type = .Deletion };
     }
 
-    // Fill the matrix - dynamic programming step
+    for (1..subj_len) |i| {
+        const idx = i * query_len + 0;
+        const gap_penalty = GAP_OPEN_PENALTY + @as(i32, @intCast(i - 1)) * GAP_EXTEND_PENALTY;
+
+        // just a placeholder as insertions only matter in actual insertion matrix
+        M[idx] = .{ .score = neg_inf, .prev_idx = 0, .prev_matrix = .M, .alignment_type = .{ .MatchMismatch = 0 } };
+        D[idx] = .{ .score = neg_inf, .prev_idx = 0, .prev_matrix = .D, .alignment_type = .Deletion };
+
+        I[idx] = .{ .score = gap_penalty, .prev_idx = (i - 1) * query_len, .prev_matrix = if (i == 1) .M else .I, .alignment_type = .{ .Insertion = i - 1 } };
+    }
+
+    // Fill the matrices - dynamic programming step
     for (1..subj_len) |i| {
         for (1..query_len) |j| {
-            const score_up = matrix[(i - 1) * query_len + j].score - 1;
-            const score_left = matrix[i * query_len + (j - 1)].score - 1;
-
+            const current_idx = i * query_len + j;
+            const diag_idx = (i - 1) * query_len + (j - 1);
+            const up_idx = (i - 1) * query_len + j;
+            const left_idx = i * query_len + (j - 1);
 
             const aa1 = std.meta.stringToEnum(AminoAcid, &[_]u8{query[j - 1]}).?;
             const aa2 = std.meta.stringToEnum(AminoAcid, &[_]u8{subj[i - 1]}).?;
             const blosum_score = BLOSUM62.getScore(aa1, aa2);
 
-            const score_diag = matrix[(i - 1) * query_len + (j - 1)].score + blosum_score;
+            // fill matrix M
+            {
+                const m_from_m = M[diag_idx].score + blosum_score;
+                const m_from_i = I[diag_idx].score + blosum_score;
+                const m_from_d = D[diag_idx].score + blosum_score;
 
-            var max_score = score_diag;
-            var prev_idx = (i - 1) * query_len + (j - 1);
-            var alignment_type: AlignmentType = .{ .MatchMismatch = i - 1 };
+                var m_max = m_from_m;
+                var m_prev_mat = MatrixState.M;
 
-            if (score_up > max_score) {
-                max_score = score_up;
-                prev_idx = (i - 1) * query_len + j;
-                alignment_type = .{ .Insertion = i - 1 };
+                if (m_from_i > m_max) {
+                    m_max = m_from_i;
+                    m_prev_mat = .I;
+                }
+                if (m_from_d > m_max) {
+                    m_max = m_from_d;
+                    m_prev_mat = .D;
+                }
+
+                M[current_idx] = .{
+                    .score = m_max,
+                    .prev_idx = diag_idx,
+                    .prev_matrix = m_prev_mat,
+                    .alignment_type = .{ .MatchMismatch = i - 1 },
+                };
             }
-            if (score_left > max_score) {
-                max_score = score_left;
-                prev_idx = i * query_len + (j - 1);
-                alignment_type = .Deletion;
+
+            // fill matrix I
+            {
+                const i_from_m = M[up_idx].score + GAP_OPEN_PENALTY;
+                const i_from_i = I[up_idx].score + GAP_EXTEND_PENALTY;
+
+                var i_max = i_from_m;
+                var i_prev_mat = MatrixState.M;
+
+                if (i_from_i > i_max) {
+                    i_max = i_from_i;
+                    i_prev_mat = .I;
+                }
+
+                I[current_idx] = .{
+                    .score = i_max,
+                    .prev_idx = up_idx,
+                    .prev_matrix = i_prev_mat,
+                    .alignment_type = .{ .Insertion = i - 1 },
+                };
             }
 
-            const current_idx = i * query_len + j;
-            matrix[current_idx] = .{
-                .score = max_score,
-                .prev_idx = prev_idx,
-                .alignment_type = alignment_type,
-            };
+            // fill matrix D
+            {
+                const d_from_m = M[left_idx].score + GAP_OPEN_PENALTY;
+                const d_from_d = D[left_idx].score + GAP_EXTEND_PENALTY;
+
+                var d_max = d_from_m;
+                var d_prev_mat = MatrixState.M;
+
+                if (d_from_d > d_max) {
+                    d_max = d_from_d;
+                    d_prev_mat = .D;
+                }
+
+                D[current_idx] = .{
+                    .score = d_max,
+                    .prev_idx = left_idx,
+                    .prev_matrix = d_prev_mat,
+                    .alignment_type = .Deletion,
+                };
+            }
         }
     }
 
+    // traceback to find optimal alignment
     const final_idx = (subj_len - 1) * query_len + (query_len - 1);
-    const final_score = matrix[final_idx].score;
 
-    var alignment_al = std.ArrayList(AlignmentType).empty;
+    // first we need to find which of the three matrices has the highest score at the bottom-right cell to start the traceback from there
+    var current_matrix = MatrixState.M;
+    var final_score = M[final_idx].score;
 
-    // Traceback to starting point to build the alignment from the matrix
-    var current_idx = final_idx;
-    while (current_idx > 0) {
-        try alignment_al.append(allocator, matrix[current_idx].alignment_type);
-        current_idx = matrix[current_idx].prev_idx;
+    if (I[final_idx].score > final_score) {
+        final_score = I[final_idx].score;
+        current_matrix = .I;
+    }
+    if (D[final_idx].score > final_score) {
+        final_score = D[final_idx].score;
+        current_matrix = .D;
     }
 
+    var alignment_al: std.ArrayList(AlignmentType) = .empty;
+
+    // traceback in 3D space of the three matrices, at each step we append the type of alignment to the alignment list and move to the previous cell in the matrix according to the prev_idx and prev_matrix stored in the current cell until we reach the top-left cell of the matrix
+    var current_idx = final_idx;
+    while (current_idx > 0) {
+        switch (current_matrix) {
+            .M => {
+                try alignment_al.append(allocator, M[current_idx].alignment_type);
+                current_matrix = M[current_idx].prev_matrix;
+                current_idx = M[current_idx].prev_idx;
+            },
+            .I => {
+                try alignment_al.append(allocator, I[current_idx].alignment_type);
+                current_matrix = I[current_idx].prev_matrix;
+                current_idx = I[current_idx].prev_idx;
+            },
+            .D => {
+                try alignment_al.append(allocator, D[current_idx].alignment_type);
+                current_matrix = D[current_idx].prev_matrix;
+                current_idx = D[current_idx].prev_idx;
+            },
+        }
+    }
+
+    // because we were iterating from final cell to start cell, we need to reverse the alignment list to get the correct order of the alignment
     std.mem.reverse(AlignmentType, alignment_al.items);
 
     return .{
@@ -217,9 +323,9 @@ fn findStarSequence(allocator: std.mem.Allocator, sequences: [][]const u8) !Star
     return star_seq;
 }
 
-fn starMethod(allocator: std.mem.Allocator, sequences: [][]const u8) ![]std.ArrayList(u8) {
+fn starMethod(allocator: std.mem.Allocator, sequences: [][]const u8, sequences_names: [][]const u8) ![]std.ArrayList(u8) {
     const star_seq = try findStarSequence(allocator, sequences);
-    std.debug.print("Best sequence index: {d} with score {d}\n", .{ star_seq.idx, star_seq.max_score });
+    std.debug.print("Best sequence: name={s}, index={d} with score {d}\n", .{sequences_names[star_seq.idx], star_seq.idx, star_seq.max_score });
 
     // align to the star sequence and build the multi-alignment by iterating through the alignments of the star sequence and inserting gaps as needed
     var pointers = try allocator.alloc(usize, sequences.len - 1);
@@ -317,7 +423,7 @@ pub fn main(init: std.process.Init) !void {
     // get input file name from command line
     const args = try init.minimal.args.toSlice(allocator);
     if (args.len < 2) {
-        std.debug.print("Usage: {s} <input_file> \n", .{args[0]});
+        std.debug.print("Usage: {s} <input_file> [gap_open_penalty] [gap_extend_penalty] \n", .{args[0]});
         return;
     }
 
@@ -326,25 +432,58 @@ pub fn main(init: std.process.Init) !void {
     const file_content = try std.Io.Dir.cwd().readFile(io, args[1], &buffer);
     var lines = std.mem.splitScalar(u8, file_content, '\n');
     var sequences_al = std.ArrayList([]const u8).empty;
+    var sequences_names_al = std.ArrayList([]const u8).empty;
     while (lines.next()) |line| {
         if (line.len == 0) continue; // skip empty lines
-        try sequences_al.append(allocator, line);
+        if (line[0] == '>') {
+            try sequences_names_al.append(allocator, line[1..]); // store sequence name without '>'
+            continue;
+        }
+        try sequences_al.append(allocator, line); // store sequence
     }
     const sequences = try sequences_al.toOwnedSlice(allocator);
+    const sequence_names = try sequences_names_al.toOwnedSlice(allocator);
+
+    // If user provided custom gap penalties, parse them from command line arguments
+    if (args.len < 4) {
+        std.debug.print("Using default gap penalties: GAP_OPEN_PENALTY = {d}, GAP_EXTEND_PENALTY = {d}\n", .{GAP_OPEN_PENALTY, GAP_EXTEND_PENALTY});
+    } else {
+        GAP_OPEN_PENALTY = try std.fmt.parseInt(i32, args[2], 10);
+        GAP_EXTEND_PENALTY = try std.fmt.parseInt(i32, args[3], 10);
+        std.debug.print("Using custom gap penalties: GAP_OPEN_PENALTY = {d}, GAP_EXTEND_PENALTY = {d}\n", .{GAP_OPEN_PENALTY, GAP_EXTEND_PENALTY});
+    }
 
     // Perform the star method to get the multi-alignment of the sequences
-    const multi_alignment = try starMethod(allocator, sequences);
+    const multi_alignment = try starMethod(allocator, sequences, sequence_names);
+
+    // find the longest sequence name for formatting the output later
+    var longest_name_len: usize = 0;
+    for (sequence_names) |name| {
+        if (name.len > longest_name_len) {
+            longest_name_len = name.len;
+        }
+    }
 
     // Write the multi-alignment results to an output file
     var sb = std.ArrayList(u8).empty;
-    for (multi_alignment) |alignment| {
+    for (multi_alignment, 0..) |alignment, i| {
         defer allocator.free(alignment.items);
+        const name = sequence_names[i];
+        try sb.appendSlice(allocator, name);
+
+        // add spaces to align the sequence names in the output + 5 spaces of padding
+        const padding_len = longest_name_len - name.len + 5;
+        try sb.appendNTimes(allocator, ' ', padding_len);
+
         try sb.appendSlice(allocator, alignment.items);
         try sb.append(allocator, '\n');
     }
 
+    const output_string = try sb.toOwnedSlice(allocator);
+    std.debug.print("\nMulti-alignment results:\n\n{s}", .{output_string});
     try std.Io.Dir.cwd().writeFile(io, .{
         .sub_path = "alignment_results.txt",
-        .data = try sb.toOwnedSlice(allocator),
+        .data = output_string,
     });
+
 }
